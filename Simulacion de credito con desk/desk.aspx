cc397401c498b8369f1ff87d7fa126ece580d50b
martinazo <33233413+martinazo@users.noreply.github.com>
2017-11-16 14:01:08 -0200

    upload proyecto simulaciones
    primer commit al subir el proyecto a github

---
+++ Simulacion de credito con desk/desk.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="desk.aspx.cs" Inherits="Default2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <!-- style del navegador! solo el navegador lateral, no de la estructura de la página-->
+    <link href="Content/nav.css" rel="stylesheet" />
+     <link href="Content/filternav.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+
+     <nav> <!-- Creamos un navegador-->
+        <ul> <!-- añadimos una lista que contendra nuestras opciones de navegación-->
+
+            <!-- agregamos nuestro elemento de lista (li) con un a href (para reenviar a algún lugar tras presionar) y una imagen-->
+            <li><a href="desk.aspx"><img src="Content/img/resources/icons/database.png" alt=""/></a></li>
+            <li><a href="graficos.aspx"><img src="Content/img/resources/icons/view-2.png" alt=""/></a></li>
+            <li><a href="logout.aspx"><img src="Content/img/resources/icons/exit.png" alt=""/></a></li>
+        </ul>
+    </nav>
+    
+   <div id="cont-selector">
+       <div id="cont-select">
+         
+          
+          <form action="/" method="post" runat="server" cssclass ="of-cont">
+        <asp:DropDownList ID="DropDownList1" cssclass="select" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="DropDownList2" cssclass="select" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="DropDownList3"  cssclass="select" runat="server"></asp:DropDownList>
+        <asp:DropDownList ID="DropDownList4" cssclass="select"  runat="server"></asp:DropDownList>
+        </form>
+         
+           <div id="filter-button"><p>Filtrar</p></div>
+       
+    </div>
+   </div>
+</body>
+</html>
---
+++ Simulacion de credito con desk/desk.aspx.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+public partial class Default2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        string Connect = ConfigurationManager.ConnectionStrings["connectuserinfo"].ConnectionString;
+        using (SqlConnection con = new SqlConnection(Connect))
+            if (Session["user"] != null)
+            {
+                // lista seleccón dropdown 1
+                string Bancos = "select nombre from ignacio.banco";
+                SqlCommand cmdBancos = new SqlCommand(Bancos, con);
+                con.Open();
+                SqlDataAdapter datosbanco = new SqlDataAdapter(cmdBancos);
+                DataTable tablabanco = new DataTable();
+                datosbanco.Fill(tablabanco);
+                DropDownList1.DataTextField = "nombre";
+                DropDownList1.DataValueField = "nombre";
+                DropDownList1.DataSource = tablabanco;
+                DropDownList1.DataBind();
+
+                DropDownList1.Items.Insert(0, "Bancos");
+                // lista seleccón dropdown 2
+                string[] valores = new string[5];
+                valores[0] = "500000";
+                valores[1] = "1999999";
+                valores[2] = "20000000";
+                valores[3] = "4999999";
+                valores[4] = "5000000";
+
+                int i = 0;
+                
+                while (true)
+                {
+                    if (i <= 1)
+                    {
+                        DropDownList2.Items.Add(valores[2 * i] + "-" + valores[2 * i + 1]);
+                    }
+
+                    else
+                    {
+                        DropDownList2.Items.Add(valores[4] + " "+ "o" +" " +"Más");
+                        break;
+                    }
+                    i++;
+                }
+                DropDownList2.Items.Insert(0, "Monto");
+
+                // lista seleccón dropdown 3
+                string[] cuotas = new string[10];
+                cuotas[0] = "12";
+                cuotas[1] = "19";
+                cuotas[2] = "20";
+                cuotas[3] = "29";
+                cuotas[4] = "30";
+                cuotas[5] = "39";
+                cuotas[6] = "40";
+                cuotas[7] = "49";
+                cuotas[8] = "50";
+                cuotas[9] = "60";
+
+                int b = 0;
+
+                while (true)
+                {
+                    if (b <= 4)
+                    {
+                        DropDownList3.Items.Add(cuotas[2 * b] + "-" + cuotas[2 * b + 1]);
+                    }
+
+                    else
+                    {
+                        
+                        break;
+                    }
+                    b++;
+                }
+
+                DropDownList3.Items.Insert(0, "Cuotas");
+
+                string[] tasa = new string[6];
+                tasa[0] = "1,0";
+                tasa[1] = "1,4";
+                tasa[2] = "1,5";
+                tasa[3] = "1,9";
+                tasa[4] = "2,0";
+                tasa[5] = "2,5";
+
+                int c = 0;
+
+                while (true)
+                {
+                    if (c <= 2)
+                    {
+                        DropDownList4.Items.Add(tasa[2 * c] + "-" + tasa[2 * c + 1]);
+                    }
+
+                    else
+                    {
+
+                        break;
+                    }
+                    c++;
+                }
+                DropDownList4.Items.Insert(0, "Tasa");
+
+            }
+            else
+        {
+            Response.Redirect("~/login.aspx");
+        }
+
+
+    }
+    protected void logout(object sender,EventArgs e)
+    {
+        
+        Response.Redirect("~/logout.aspx");
+    }
+
+   
+}
